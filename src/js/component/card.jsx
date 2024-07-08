14fd85ba77d257844ee34dd55c48529a27e0d6ac
jeff-c44 <156033827+jeff-c44@users.noreply.github.com>
2024-07-08 19:24:37 +0000

    final

---
+++ src/js/component/card.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+const Card = () => {
+    return(
+      <div className="row row-cols-3 ">
+  <div className="col-3">
+    <div className="card">
+      <img src="https://picsum.photos/200" className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero dolorem at vero, eum harum pariatur facere officiis excepturi vel iure voluptate, deleniti animi ut quo quibusdam debitis voluptatum hic! Ad.</p>
+        <div class="card-footer">
+        <a href="#" class="btn btn-primary">Find Out More!</a>
+      </div>
+      </div>
+    </div>
+  </div>
+  <div className="col-3">
+    <div className="card">
+      <img src="https://picsum.photos/200" className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae mollitia magni culpa in magnam deserunt sapiente ea necessitatibus earum libero nobis a eum voluptatibus quibusdam, fuga atque quia tempore tempora!</p>
+        <div class="card-footer">
+        <a href="#" class="btn btn-primary">Find Out More!</a>
+      </div>
+      </div>
+    </div>
+  </div>
+  <div className="col-3">
+    <div className="card">
+      <img src="https://picsum.photos/200" className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae mollitia magni culpa in magnam deserunt sapiente ea necessitatibus earum libero nobis a eum voluptatibus quibusdam, fuga atque quia tempore tempora!</p>
+        <div class="card-footer">
+        <a href="#" class="btn btn-primary">Find Out More!</a>
+      </div>
+      </div>
+    </div>
+  </div>
+  <div className="col-3">
+    <div className="card">
+      <img src="https://picsum.photos/200" className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima enim eum in recusandae similique itaque quas quam iure illo culpa magni, error eaque dolorum nam quasi eligendi iusto repudiandae aspernatur.</p>
+        <div class="card-footer">
+        <a href="#" class="btn btn-primary">Find Out More!</a>
+      </div>
+      </div>
+    </div>
+  </div>
+</div>
+      )
+}
+export default Card
---
+++ src/js/component/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Footer = () => {
+    return(
+        <div className="col">
+            <div className="row bg-dark bg-gradient text-white text-center">
+                <p>Copyright © Your Website 2024</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
